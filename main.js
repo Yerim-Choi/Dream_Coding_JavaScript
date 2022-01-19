1a2458ce3febdceea1ebe005f8b84ccd01c8e0b2
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-01-19 22:43:23 +0900

    Dream_Coding_JavaScript Note

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-//JavaScript is very fiexible
+//JavaScript is very fiexible 자바스크립트는 유연한 언어
+// fiexible === dangerous >> 개발자의 많은 실수가 가능
+// added ECMAScript 5
+// strict 모드 실행 >> 자바스크립트 엔진이 조금 더 효율적으로 빠르게 실행할 수 있음
 'use-strict';
 
+//node main.js 실행
 console.log('Hello world!');
